1d78911d2e4e44f58a0126c5d6dbaccaeb8bcaa7
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2018-01-18 00:18:33 -0200

    teste de atualização e remoção de categoria - refatoração de testes

--- src/services/mysql/teste/categories.test.js
+++ src/services/mysql/teste/categories.test.js
@@ -4,7 +4,25 @@ const test = require('ava')
 const { connection, errorHandler } = require('./setup')
 const categories = require('../categories')({ connection, errorHandler })
 
+test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
+test.after.always(t => connection.query('TRUNCATE TABLE categories'))
+
+const createCategory = () => categories.save('category-test')
+
 test('Deve criar uma categoria', async t => {
-    const result = await categories.save('category-test')
+    const result = await createCategory()
     t.is(result.categories.name, 'category-test')
+})
+
+test('Deve atualizar uma categoria', async t => {
+    await createCategory()
+    const result = await categories.update(1, 'category-test-updated')
+    t.is(result.categories.name, 'category-test-updated')
+    t.is(result.affectedRows, 1)
+})
+
+test('Deve remover uma categoria', async t => {
+    await createCategory()
+    const result = await categories.del(1)
+    t.is(result.affectedRows, 1)
 })
