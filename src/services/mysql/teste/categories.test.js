77d29b51d87cdb84adf6cad028261443b511b0ad
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2018-01-18 00:33:09 -0200

    teste de listagem de listar categorias e rodanco como serial

--- src/services/mysql/teste/categories.test.js
+++ src/services/mysql/teste/categories.test.js
@@ -7,22 +7,29 @@ const categories = require('../categories')({ connection, errorHandler })
 test.beforeEach(t => connection.query('TRUNCATE TABLE categories'))
 test.after.always(t => connection.query('TRUNCATE TABLE categories'))
 
-const createCategory = () => categories.save('category-test')
+const create = () => categories.save('category-test')
+
+test('Deve listar categorias', async t => {
+    await create()
+    await create()
+    const list = await categories.all()
+    t.is(list.categories.length, 2)
+})
 
 test('Deve criar uma categoria', async t => {
-    const result = await createCategory()
+    const result = await create()
     t.is(result.categories.name, 'category-test')
 })
 
 test('Deve atualizar uma categoria', async t => {
-    await createCategory()
+    await create()
     const result = await categories.update(1, 'category-test-updated')
     t.is(result.categories.name, 'category-test-updated')
     t.is(result.affectedRows, 1)
 })
 
 test('Deve remover uma categoria', async t => {
-    await createCategory()
+    await create()
     const result = await categories.del(1)
     t.is(result.affectedRows, 1)
 })
